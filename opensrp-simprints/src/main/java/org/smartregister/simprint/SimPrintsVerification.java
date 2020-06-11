745ffe854518e9d73486db2b6ddb48c7558bd192
issyzac <issyzac.iz@gmail.com>
2020-06-11 22:58:17 +0300

    Refactored to include a tier representation that will be accessible where library is implemented

--- opensrp-simprints/src/main/java/org/smartregister/simprint/SimPrintsVerification.java
+++ opensrp-simprints/src/main/java/org/smartregister/simprint/SimPrintsVerification.java
@@ -8,10 +8,11 @@ public class SimPrintsVerification implements Serializable {
     private String guid;
     private Boolean checkStatus;
     private Tier tier;
+    private MaskedTier maskedTier;
+
     public SimPrintsVerification(String guId){
         this.guid = guId;
     }
-
 
     public void setTier(Tier tier) {
         this.tier = tier;
@@ -24,6 +25,7 @@ public class SimPrintsVerification implements Serializable {
     public String getGuid() {
         return guid;
     }
+
     public Boolean getCheckStatus() {
         return checkStatus;
     }
@@ -31,4 +33,22 @@ public class SimPrintsVerification implements Serializable {
     public void setCheckStatus(Boolean checkStatus) {
         this.checkStatus = checkStatus;
     }
+
+    public void setMaskedTier(MaskedTier maskedTier) {
+        this.maskedTier = maskedTier;
+    }
+
+    public MaskedTier getMaskedTier() {
+        return maskedTier;
+    }
+
+    enum MaskedTier {
+        TIER_1,
+        TIER_2,
+        TIER_3,
+        TIER_4,
+        TIER_5,
+    }
 }
+
+
